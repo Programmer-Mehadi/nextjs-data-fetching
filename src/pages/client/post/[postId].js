052039eda508916dc62be556ce2bd5a388a44f75
Programmer-Mehadi <mehadi.developer@gmail.com>
2023-07-23 10:37:37 +0600

    project setup

---
+++ src/pages/client/post/[postId].js
@@ -0,0 +1,28 @@
+import { useRouter } from 'next/router'
+import React, { useEffect } from 'react'
+
+const SinglePostPage = () => {
+  const id = useRouter()?.query?.postId;
+  const [post, setPost] = React.useState([])
+  useEffect(() => {
+    const url = `https://jsonplaceholder.typicode.com/posts/${id}`;
+    async function fetchData() {
+      const res = await fetch(url);
+      const data = await res.json();
+      setPost(data)
+    }
+    fetchData()
+  }, [id])
+  console.log(post)
+  return (
+    <div>
+      <p className='mt-8 font-bold text-2xl text-center'>Single Post Page <span className='text-emerald-600'>[Client side Fetching]</span></p>
+
+      <p className='text-center mt-10'>User ID: {post.userId}</p>
+      <p className='text-center'>Title: {post.title}</p>
+      <p className='text-center'>Body: {post.body}</p>
+    </div>
+  )
+}
+
+export default SinglePostPage
---
+++ src/pages/static/post/[postId].js
@@ -0,0 +1,37 @@
+import React, { useEffect } from 'react'
+
+const SinglePostPage = ({ post }) => {
+  return (
+    <div>
+      <p className='mt-8 font-bold text-2xl text-center'>Single Post Page <span className='text-emerald-600'>[Client side Fetching]</span></p>
+      <p className='text-center mt-10'>User ID: {post.userId}</p>
+      <p className='text-center'>Title: {post.title}</p>
+      <p className='text-center'>Body: {post.body}</p>
+    </div>
+  )
+}
+
+export default SinglePostPage;
+
+export const getStaticPaths = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+  const paths = data.map((post) => ({
+    params: { postId: post.id.toString() }
+  }));
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+
+export const getStaticProps = async (context) => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.postId}`);
+  const data = await res.json();
+  return {
+    props: {
+      post: data
+    }
+  }
+}
