961326602f0e3b44343d42a1775d8915cad2949d
Programmer-Mehadi <mehadi.developer@gmail.com>
2023-07-23 14:54:54 +0600

    api change.

--- src/redux/api/api.js
+++ src/redux/api/api.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const apiSlice = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000/api' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://nextjs-data-fetching.netlify.app/api' }),
   endpoints: builder => ({
     getPosts: builder.query({
       query: () => '/post'
